575547de0c70629963570bdfdddb55d8444e7feb
hoangnguyennn <nvanhoang188@gmail.com>
2024-07-22 14:37:29 +0700

    fix persist key warning

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,23 @@
-import { persistReducer, persistStore } from 'redux-persist'
+import { PersistConfig, persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 import { configureStore } from '@reduxjs/toolkit'
 
 import rootReducer from './reducers'
 
-const persistConfig = {
+const persistConfig: PersistConfig<Store.IRootState> = {
   key: 'root',
   storage
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = configureStore({ reducer: persistedReducer })
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({ serializableCheck: false })
+  }
+})
 
 export const persistor = persistStore(store)
 
