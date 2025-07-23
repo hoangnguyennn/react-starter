13077db2c0fcb90bf6edb4091b10e0946a2ca4db
hoangnguyennn <nvanhoang188@gmail.com>
2025-07-23 13:25:38 +0700

    fix ci

--- test/provider.tsx
+++ test/provider.tsx
@@ -1,7 +1,6 @@
 import '@hn/locales'
 import store, { persistor } from '@hn/store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { FC, PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
